dc5f89cd2d714c005cc1df3e50aeb843ca88deef
NaeemMeyer <ST10468609@rcconnect.edu.za>
2026-06-25 22:55:59 +0200

    Create QuizModels.cs

---
+++ QuizModels.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ST10468609_Mogamat_Naeem_Meyer_PROG6221
+{
+    public class QuizQuestion
+    {
+        public string QuestionText { get; set; }
+        public List<string> Options { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Explanation { get; set; }
+    }
+}
